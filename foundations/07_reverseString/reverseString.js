ba934aa659c8d2770ad0e2461dcb33977664a652
Prerna <prernakumarisharma2003@gmail.com>
2026-04-29 14:01:39 +0530

    solved exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let  length=string.length;
+    let final_string="";
+    for (let i=length-1;i>=0;i--){
+           final_string=final_string+string[i];
+    }
+    return final_string;
 
 };
 
