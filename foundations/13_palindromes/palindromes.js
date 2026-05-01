d3afe3d67c83ad9198842d981854c797dbccedf1
Prerna <prernakumarisharma2003@gmail.com>
2026-05-01 14:22:52 +0530

    solved exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let fs=string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    fstr=fs.replace(/\s/g, "");
+    console.log(fstr);
+    let rev='';
+    le=fstr.length;
+    for (let i=le-1;i>=0;i--){
+        rev=rev+fstr[i];
+    }
+    console.log(rev);
+    if (rev.toUpperCase()===fstr.toUpperCase()){
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
 
