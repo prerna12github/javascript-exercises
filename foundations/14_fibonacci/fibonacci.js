81176a6ef0753782027a98accd2f730785b4bfb1
Prerna <prernakumarisharma2003@gmail.com>
2026-05-02 00:32:03 +0530

    solved exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a=1;
+    let b=1;
+    let c=a+b;
+    let fib=[];
+    fib.push(a,b,c);
+    if(num<0){
+        return "OOPS"
+    }
+    else if(num==0){
+        return 0;
+    }
+    for (let i=2;i<num;i++){
+        a=b;
+        b=c;
+        c=a+b;
+        fib.push(c);
+    }
+    return fib[num-1];
 };
 
 // Do not edit below this line
