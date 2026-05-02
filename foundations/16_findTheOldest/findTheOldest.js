a28700a0a26a0bb2099ef0d9810114878e7d4508
Prerna <prernakumarisharma2003@gmail.com>
2026-05-02 13:14:50 +0530

    solved exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+      let oldest = {};
+  let oldestAge = 0;
+  for (let i = 0; i < people.length; i++) {
+    if (!people[i].yearOfDeath){
+        const d = new Date();
+        let year=d.getFullYear();
+        people[i].yearOfDeath=year;
+    }
+    let age = people[i].yearOfDeath - people[i].yearOfBirth
+    if( age >= oldestAge) {
+       oldestAge = age;
+       oldest = people[i];
+    }
+  }
+  return oldest;
+}
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
